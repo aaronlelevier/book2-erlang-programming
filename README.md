7c42aa6023b5681191cf26c67d49124f50a8a34d
aaronlelevier <aaron.lelevier@gmail.com>
2020-01-06 06:28:29 -0800

    first commit
    Erlang Programming by Francesco Cesarini - exercises from book

---
+++ README.md
@@ -0,0 +1,9 @@
+book2
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ apps/book2/src/book2_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc book2 public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(book2_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    book2_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
