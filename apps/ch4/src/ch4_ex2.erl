a71cd6e74fac4be583926071caafe59f8ecf83dd
aaronlelevier <aaron.lelevier@gmail.com>
2020-01-24 06:21:27 -0800

    ch4 ex2 - process ring exercise
    create N processes and send a Msg around the ring M number of times

--- apps/ch4/src/ch4_ex2.erl
+++ apps/ch4/src/ch4_ex2.erl
@@ -8,12 +8,56 @@
 %%%-------------------------------------------------------------------
 -module(ch4_ex2).
 -author("aaron lelevier").
--export([start/3]).
+-export([start/3, loop/0]).
+-include_lib("book2/include/macros.hrl").
 
 %% @doc starts a ring of N processes and sends the Message
 %% around the ring M number of times
--spec start(M::integer(), N::integer(), Message::string()).
-start(M, N, Message) -> {M, N, Message}.
+-spec start(M::integer(), N::integer(), Msg ::string()) -> any().
+start(M, N, Msg) ->
+  ?DEBUG({M, N, Msg}),
+  PidList = pidlist(N),
+  [H|T] = PidList,
+  H ! {Msg, T, PidList, 1, M}.
+
+pidlist(N) ->
+  [spawn(?MODULE, loop, []) || _X <- lists:seq(1, N)].
+
+loop() ->
+  receive
+    {quit, CurPidList, PidList, CurM, M}=QuitMsg ->
+      ?DEBUG({self(), QuitMsg}),
+      ?DEBUG({self(), quitting}),
+      case CurPidList of
+        [] ->
+          ?DEBUG({self(), "all Pids have quite"}),
+          done;
+        [H|T] ->
+          ?DEBUG({self(), "in sequence of quits, tell next Pid to quit"}),
+          H ! {quit, T, PidList, CurM, M}
+      end;
+    {Msg, CurPidList, PidList, CurM, M}=FwdMsg ->
+      ?DEBUG({self(), FwdMsg}),
+      case CurPidList of
+        [] ->
+          if
+            CurM =:= M ->
+              ?DEBUG({self(), "initialize sequence of quits"}),
+              [H|T] = PidList,
+              H ! {quit, T, PidList, CurM, M},
+              loop();
+            true ->
+              ?DEBUG({self(), "incr M and start new round of Msg's"}),
+              [H|T] = PidList,
+              H ! {Msg, T, PidList, CurM+1, M},
+              loop()
+          end;
+        [H|T] ->
+          ?DEBUG({self(), "same round send to next Pid in CurPidList"}),
+          H ! {Msg, T, PidList, CurM, M},
+          loop()
+      end
+  end.
 
 %% TODO: A - have a central process that sets up the ring and
 %% initiates message sending
