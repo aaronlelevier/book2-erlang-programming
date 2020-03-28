6b00343f844dfc678a85b413a1eac69065b8940a
aaronlelevier <aaron.lelevier@gmail.com>
2020-03-28 08:17:01 -0700

    ch9 ex2 - list comprehension exercises

---
+++ apps/ch9/src/ch9_ex2.erl
@@ -0,0 +1,35 @@
+%%%-------------------------------------------------------------------
+%%% @author aaron lelevier
+%%% @doc
+%%% List comprehensions
+%%% @end
+%%% Created : 28. Mar 2020 7:27 AM
+%%%-------------------------------------------------------------------
+-module(ch9_ex2).
+-author("aaron lelevier").
+-vsn(1.0).
+-export([
+  divisible_by/2, integers_squared/1, intersection/2,
+  symmetric_diff/2, zip/2, zipWith/3]).
+
+divisible_by(N, L) -> [X || X <- L, X rem N =:= 0].
+
+integers_squared(L) -> [X * X || X <- L, is_integer(X)].
+
+intersection(L1, L2) ->
+  [X || X <- L1, Y <- L2, X =:= Y].
+
+symmetric_diff(L1, L2) ->
+  lists:merge(
+    [X || X <- L1, not lists:member(X, L2)],
+    [Y || Y <- L2, not lists:member(Y, L1)]
+  ).
+
+zip(L1, L2) ->
+  [{lists:nth(N, L1), lists:nth(N, L2)} || N <- lists:seq(1, length(L1))].
+
+zipWith(Fun, L1, L2) ->
+  [
+    Fun(lists:nth(N, L1), lists:nth(N, L2))
+    || N <- lists:seq(1, length(L1))
+  ].
---
+++ test/unit/ch9/ch9_ex2_tests.erl
@@ -0,0 +1,59 @@
+%%%-------------------------------------------------------------------
+%%% @author aaron lelevier
+%%% @doc
+%%%
+%%% @end
+%%% Created : 28. Mar 2020 7:27 AM
+%%%-------------------------------------------------------------------
+-module(ch9_ex2_tests).
+-author("aaron lelevier").
+-compile(nowarn_export_all).
+-include_lib("eunit/include/eunit.hrl").
+
+divisible_by_test() ->
+  N = 3,
+  L = lists:seq(1, 10),
+
+  Ret = ch9_ex2:divisible_by(N, L),
+
+  ?assertEqual([3, 6, 9], Ret).
+
+integers_squared_test() ->
+  L = [1, hello, 100, boo, "boo", 9],
+
+  Ret = ch9_ex2:integers_squared(L),
+
+  ?assertEqual([1, 10000, 81], Ret).
+
+intersection_test() ->
+  L1 = [1, 2, 3, 4, 5],
+  L2 = [4, 5, 6, 7, 8],
+
+  Ret = ch9_ex2:intersection(L1, L2),
+
+  ?assertEqual([4, 5], Ret).
+
+symmetric_diff_test() ->
+  L1 = [1, 2, 3, 4, 5],
+  L2 = [4, 5, 6, 7, 8],
+
+  Ret = ch9_ex2:symmetric_diff(L1, L2),
+
+  ?assertEqual([1, 2, 3, 6, 7, 8], Ret).
+
+zip_test() ->
+  L1 = [1, 2],
+  L2 = [3, 4, 5],
+
+  Ret = ch9_ex2:zip(L1, L2),
+
+  ?assertEqual([{1, 3}, {2, 4}], Ret).
+
+zipWith_test() ->
+  Add = fun(X, Y) -> X + Y end,
+  L1 = [1, 2],
+  L2 = [3, 4, 5],
+
+  Ret = ch9_ex2:zipWith(Add, L1, L2),
+
+  ?assertEqual([4, 6], Ret).
