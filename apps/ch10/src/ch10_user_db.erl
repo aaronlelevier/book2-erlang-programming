156a7d72abd77cd4369163210f33a67868b2f50d
aaronlelevier <aaron.lelevier@gmail.com>
2020-04-09 07:52:29 -0700

    ch10 usr db - delete disabled functions and ct test

--- apps/ch10/src/ch10_user_db.erl
+++ apps/ch10/src/ch10_user_db.erl
@@ -11,7 +11,8 @@
 -export([
   create_tables/1, close_tables/0, restore_tables/0,
   add_usr/1, update_usr/1,
-  lookup_id/1, lookup_msisdn/1, get_index/1]).
+  lookup_id/1, lookup_msisdn/1, get_index/1,
+  delete_disabled/0, delete_usr/2]).
 
 -include("usr.hrl").
 
@@ -19,7 +20,7 @@
 
 create_tables(FileName) ->
   ets:new(usrRam, [named_table, {keypos, #usr.msisdn}]),
-  ets:new(usrIndex, [named_table]),
+  ets:new(usrIndex, [named_table]), % keypos: CustId
   dets:open_file(
     usrDisk, [{file, FileName}, {keypos, #usr.msisdn}]).
 
@@ -65,3 +66,27 @@ lookup_msisdn(PhoneNo) ->
     [Usr] -> {ok, Usr};
     [] -> {error, instance}
   end.
+
+%% Delete Usr Functions
+
+delete_disabled() ->
+  ets:safe_fixtable(usrRam, true),
+  catch loop_delete_disabled(ets:first(usrRam)),
+  ets:safe_fixtable(usrRam, false),
+  ok.
+
+loop_delete_disabled('$end_of_table') ->
+    ok;
+loop_delete_disabled(PhoneNo) ->
+  case ets:lookup(usrRam, PhoneNo) of
+    [#usr{status = disabled, id = CustId}] ->
+      delete_usr(PhoneNo, CustId);
+    _ ->
+      ok
+  end,
+  loop_delete_disabled(ets:next(usrRam, PhoneNo)).
+
+delete_usr(PhoneNo, CustId) ->
+  true = ets:delete(usrRam, PhoneNo),
+  true = ets:delete(usrIndex, CustId),
+  ok = dets:delete(usrDisk, PhoneNo).
--- test/integ/ch10/ch10_user_db_SUITE.erl
+++ test/integ/ch10/ch10_user_db_SUITE.erl
@@ -13,7 +13,8 @@
 
 all() -> [
   add_and_update_usr,
-  can_lookup_and_get_index
+  can_lookup_and_get_index,
+  can_delete_disabled_users
 ].
 
 add_and_update_usr(_) ->
@@ -47,5 +48,35 @@ can_lookup_and_get_index(_) ->
   % get_index
   {ok, 456} = ch10_user_db:get_index(2),
   {error, instance} = ch10_user_db:get_index(-123),
+
+  ch10_user_db:close_tables().
+
+can_delete_disabled_users(_) ->
+  ch10_user_db:create_tables("usr_db_ct.dets"),
+
+  Usr1 = #usr{msisdn = 123, id = 1, status = enabled},
+  Usr2 = #usr{msisdn = 456, id = 2, status = enabled},
+
+  ch10_user_db:add_usr(Usr1),
+  ch10_user_db:add_usr(Usr2),
+
+  % users are present
+  2 = ets:info(usrRam, size),
+
+  % explicit delete
+  ch10_user_db:delete_usr(123, 1),
+
+  1 = ets:info(usrRam, size),
+  {error, instance} = ch10_user_db:lookup_id(1),
+  {ok, Usr2} = ch10_user_db:lookup_id(2),
+
+  % disable a usr
+  ch10_user_db:update_usr(Usr2#usr{status = disabled}),
+
+  % delete disabled users
+  ch10_user_db:delete_disabled(),
+
+  % check deleted
+  0 = ets:info(usrRam, size),
 
   ch10_user_db:close_tables().
