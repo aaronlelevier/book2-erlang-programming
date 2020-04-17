4554bcbebe1da90a27470ca9e140a59827a26eff
aaronlelevier <aaron.lelevier@gmail.com>
2020-04-17 06:26:34 -0700

    ch10 ex1 exercises

--- apps/ch10/src/ch10_ex1.erl
+++ apps/ch10/src/ch10_ex1.erl
@@ -8,7 +8,7 @@
 -module(ch10_ex1).
 -author("aaron lelevier").
 -vsn(1.0).
--export([accumulate/1]).
+-export([accumulate/1, prettyList/1, pad/2]).
 
 accumulate(L) ->
   % unique
@@ -17,21 +17,44 @@ accumulate(L) ->
   L3 = lists:sort(L2),
   accumulate(L3, undefined, []).
 
-accumulate([], Item, Acc) -> lists:reverse([Item|Acc]);
-accumulate([H|T], Item, Acc) ->
+accumulate([], Item, Acc) -> lists:reverse([Item | Acc]);
+accumulate([H | T], Item, Acc) ->
   case Item of
     undefined ->
       accumulate(T, {H}, Acc);
     {X} ->
       {Item2, Acc2} = if
-        H - 1 == X -> {{X, H}, Acc};
-        true -> {{H}, [{X}|Acc]}
-      end,
+                        H - 1 == X -> {{X, H}, Acc};
+                        true -> {{H}, [{X} | Acc]}
+                      end,
       accumulate(T, Item2, Acc2);
     {X, Y} ->
       {Item2, Acc2} = if
-        H - 1 == Y -> {{X, H}, Acc};
-        true -> {{H}, [{X, Y}|Acc]}
-      end,
+                        H - 1 == Y -> {{X, H}, Acc};
+                        true -> {{H}, [{X, Y} | Acc]}
+                      end,
       accumulate(T, Item2, Acc2)
   end.
+
+%% take the results of accumulate/1 and convert to string output
+prettyList(L) -> prettyList(L, []).
+
+prettyList([], Acc) ->
+  L2 = lists:join(",",  lists:reverse(Acc)),
+  concat_str(L2, "");
+prettyList([{X} | T], Acc) ->
+  S1 = integer_to_list(X),
+  prettyList(T, [S1 | Acc]);
+prettyList([{X, Y} | T], Acc) ->
+  S1 = integer_to_list(X),
+  S2 = integer_to_list(Y),
+  prettyList(T, [S1 ++ "-" ++ S2 | Acc]).
+
+concat_str([], Str) -> Str;
+concat_str([H|T], Str) ->
+  concat_str(T, Str ++ H).
+
+%% pads a Word to the right by N, given N is length than the
+%% length of the string
+pad(N, Word) when length(Word) > N -> Word;
+pad(N, Word) -> string:right(Word, N, 32).
--- test/unit/ch10/ch10_ex1_tests.erl
+++ test/unit/ch10/ch10_ex1_tests.erl
@@ -14,3 +14,12 @@ accumulate_test() ->
   L = [7,6,6,5,3,3,1,1],
   Ret = ch10_ex1:accumulate(L),
   ?assertEqual([{1}, {3}, {5,7}], Ret).
+
+prettyList_test() ->
+  ?assertEqual("1,3,5-7", ch10_ex1:prettyList([{1}, {3}, {5,7}])).
+
+pad_n_is_lt_the_word_test() ->
+  ?assertEqual("foo", ch10_ex1:pad(2, "foo")).
+
+pad_n_is_gt_the_word_test() ->
+  ?assertEqual("  go", ch10_ex1:pad(4, "go")).
