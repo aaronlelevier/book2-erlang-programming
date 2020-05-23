498f0a0d078b44cfef372517fc44373649a551e4
aaronlelevier <aaron.lelevier@gmail.com>
2020-05-23 09:19:01 -0700

    all functions in a Monad sequence must either obey the method signature of the Monad or be wrapped in the 'return' function that formats the value returned for the next function

--- apps/monads/src/monad_learning.erl
+++ apps/monads/src/monad_learning.erl
@@ -5,12 +5,13 @@
 %%% @end
 %%% Created : 23. May 2020 7:15 AM
 %%%-------------------------------------------------------------------
--module(monads_learning).
+-module(monad_learning).
 -author("Aaron Lelevier").
 -vsn(1.0).
--export([is_integers/1, do_identity/1, rand_int/0, if_safe_div_zero/3]).
+-export([is_integers/1, do_identity/1, rand_int/0, if_safe_div_zero/3,
+  if_unsafe_div_zero/2, log/1]).
 -compile({parse_transform, do}).
-
+-include_lib("book2/include/macros.hrl").
 
 %% @doc checks a list if each item is an integer and returns a list of booleans
 -spec is_integers(L :: [any()]) -> [boolean()].
@@ -28,16 +29,35 @@ do_identity(X) ->
 rand_int() ->
   do([identity_m ||
     X <- rand:normal(),
-    X2 <- X*100000,
+    X2 <- X * 100000,
     X3 <- trunc(X2),
     abs(X3)]).
 
 %% Maybe Monad
 
+%% @doc example usage: monad_learning:if_safe_div_zero(8, 4, fun(X) -> X + 1 end).
 if_safe_div_zero(X, Y, Fun) ->
   do([maybe_m ||
     Result <- case Y == 0 of
-                true  -> fail("Cannot divide by zero");
+                true -> fail("Cannot divide by zero");
                 false -> return(X / Y)
               end,
+    % must apply `return` to format as: {just, X}
+    return(log(Result)),
+    % apply Fun and return
     return(Fun(Result))]).
+
+
+%% won't hit the false clause if Y == 0, but maybe the above is better
+%% because it allows prettier chaining
+if_unsafe_div_zero(X, Y) ->
+  case Y == 0 of
+    true -> 0;
+    false -> X / Y
+  end.
+
+
+%% Utility Functions
+log(X) ->
+  ?LOG(X),
+  X.
